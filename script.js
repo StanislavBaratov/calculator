a00631c7078dced75b714b4b637a87b45733e152
StanislavBaratov <stanislavbaratov@mail.ru>
2025-03-17 23:37:49 +0500

    Add digit input

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ const FIRST_OPERAND = 0;
 const SECOND_OPERAND = 1;
 
 const calculator = {
-    display: document.querySelector('.display'),
+    display: document.querySelector('input'),
     buttons: document.querySelectorAll('button'),
     firstOperand: 0,
     secondOperand: 0,
     state: FIRST_OPERAND,
     currentOperator: null,
 
+    addDigit: function(digit) {
+        console.log(this.display);
+        this.display.value += digit;
+    },
+
     keyPressed: function(event) {
-        console.log(event.target.id)
+        console.log(event.target.id);
+        console.log(event.target.className);
+        if (event.target.className === 'digit') {
+            calculator.addDigit(event.target.id);
+        }
     },
 
     resetCalculator: function() {
